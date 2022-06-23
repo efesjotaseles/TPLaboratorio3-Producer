9d868c95ac407b4730b66f5a7f69af9e75919770
efesjotaseles <efesjotaseles@gmail.com>
2022-06-23 15:19:12 -0300

    Possible solution for getting Class from the JSON

--- src/App/App.java
+++ src/App/App.java
@@ -1,12 +1,48 @@
 package App;
 
 
+//import ElementClasses.BaseMaterials.Metal;
+
+import ElementClasses.BaseMaterials.BaseMaterial;
+
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
 
+        Class c;
+        Object o;
+        BaseMaterial baseMaterial;
+        //Deprecated way
+        /*
+        try {
+            c = Class.forName("ElementClasses.BaseMaterials.Metal");
+            o = c.newInstance();
+            baseMaterial = (BaseMaterial) o;
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+         */
 
+        try {
+            c = Class.forName("ElementClasses.BaseMaterials.Metal");
+            o = c.getDeclaredConstructor().newInstance();
+            baseMaterial = (BaseMaterial) o;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println(baseMaterial.toString());
 
 
 
