7718682c41decf399afeb6ecd0a518c41f7e68bb
efesjotaseles <efesjotaseles@gmail.com>
2022-06-24 02:04:03 -0300

    Add marketMenu, marketBuyMenu and marketSellMenu

--- src/Console/Console.java
+++ src/Console/Console.java
@@ -1,5 +1,7 @@
 package Console;
 
+import java.util.ArrayList;
+
 public class Console {
 
     private static void divScreen(){
@@ -28,14 +30,48 @@ public class Console {
     public static void dailyMenu(int[] data, int[]tradeActions, int[] fabricateActions){
         divScreen();
         info(data);
+        divScreen();
         System.out.println("1) Market (" + tradeActions[0] + "/" + tradeActions[1] + ")");
         System.out.println("2) Fabricate (" + fabricateActions[0] + "/" + fabricateActions[1] + ")");
         System.out.println("3) Manage Machines");
         System.out.println("4) End the day");
         System.out.println("5) Pay debt!");
         System.out.println("0) Save & Close");
+        divScreen();
     }
 
-    public static void marketMenu()
+    public static void marketMenu(int[] data){
+        divScreen();
+        info(data);
+        divScreen();
+        System.out.println("1) Buy...");
+        System.out.println("2) Sell...");
+        System.out.println("0) Go back");
+    }
+
+    /**
+     *
+     * @param data [currentDay, dueDay, currentCash, goalCash]
+     * @param offer
+     */
+    public static void marketBuyMenu(int[] data, ArrayList<String>offer){
+        divScreen();
+        info(data);
+        divScreen();
+        System.out.println("1) " + offer.get(0));
+        System.out.println("2) " + offer.get(1));
+        System.out.println("3) " + offer.get(2));
+        System.out.println("4) " + offer.get(3));
+        System.out.println("0) Go back");
+        divScreen();
+    }
+
+    public static void marketSellMenu(int[] data){
+        divScreen();
+        info(data);
+        divScreen();
+        System.out.println("");
+        System.out.println("0) Go back");
+    }
 
 }
